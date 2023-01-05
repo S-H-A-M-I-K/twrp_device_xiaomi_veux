984cbdcd9d76364411f63db7c3d4a98bba2240a3
S-H-A-M-I-K <106610972+S-H-A-M-I-K@users.noreply.github.com>
2023-01-05 22:06:41 +0530

    Update device.mk

--- twrp_device_xiaomi_veux/device.mk
+++ twrp_device_xiaomi_veux/device.mk
@@ -93,5 +93,5 @@ TW_RECOVERY_ADDITIONAL_RELINK_LIBRARY_FILES += \
 	$(TARGET_OUT_SHARED_LIBRARIES)/libion.so \
 	$(TARGET_OUT_SHARED_LIBRARIES)/libxml2.so \
     $(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/vendor.display.config@1.0.so \
-    $(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/vendor.display.config@2.0.so
-
+    $(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/vendor.display.config@2.0.so \
+    $(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/libdisplayconfig.qti.so
